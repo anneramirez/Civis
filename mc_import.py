487d519b18ae6466800ef7d89ff9b31936ca6135
anneramirez <anne.ramirez@ppfa.org>
2019-05-29 16:34:32 -0400

    Create mc_import.py

---
+++ mc_import.py
@@ -0,0 +1,25 @@
+import xml2json
+import requests
+import civis
+#import json
+#import os
+#from requests.auth import HTTPBasicAuth
+#import pandas
+#import xml.etree.ElementTree as ET
+
+url = "https://secure.mcommons.com/api/profiles"
+user = "anne.ramirez@ppfa.org"
+pw = "Dre$m0B0$t"
+querystring = {"include_custom_columns":"false",
+               "include_subscriptions":"false",
+              "include_clicks":"false",
+              "include_members":"false",
+              "limit":"10"}
+
+payload = ""
+headers = {'Content-type' : 'application/json'}
+
+r = requests.request("GET", url, auth = HTTPBasicAuth(user,pw), data=payload, headers=headers, params=querystring)
+root = ET.fromstring(r.text)
+print(root)
+print xml2json.xml2json(r.text)
