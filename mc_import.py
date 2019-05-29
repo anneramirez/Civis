f0a12db20d4a62d278f91ee7f5a3d187f0382571
anneramirez <anne.ramirez@ppfa.org>
2019-05-29 19:54:38 -0400

    Update mc_import.py

--- mc_import.py
+++ mc_import.py
@@ -24,35 +24,31 @@ xml_data = r.content
 
 class XML2DataFrame:
 
-    def __init__(self, xml_data):
-        self.root = ET.XML(xml_data)
-
-    def parse_root(self, root):
-        #Return a list of dictionaries from the text and attributes of the children under this XML root.#
-        for child in root.getchildren():
-          return parse_element(child)
-                
-        def parse_element(self, element, parsed=None):
-        #Collect {key:attribute} and {tag:text} from thie XML element and all its children into a single dictionary of strings.#
-          if parsed is None:
-              parsed = dict()
-          for key in element.keys():
-              if key not in parsed:
-                 parsed[key] = element.attrib.get(key)
-              if element.text:
-                 parsed[element.tag] = element.text
-              else:
-                  raise ValueError('duplicate attribute {0} at element {1}'.format(key, element.getroottree().getpath(element)))           
-
-       # Apply recursion #
-        for child in list(element):
-            self.parse_element(child, parsed)
+  def __init__(self, xml_data):
+    self.root = ET.XML(xml_data)
+  def parse_root(self, root):
+    def parse_element(self, element, parsed=None):
+      #Collect {key:attribute} and {tag:text} from thie XML element and all its children into a single dictionary of strings.
+      if parsed is None:
+        parsed = dict()
+      for key in element.keys():
+        if key not in parsed:
+          parsed[key] = element.attrib.get(key)
+        if element.text:
+          parsed[element.tag] = element.text
+        else:
+          raise ValueError('duplicate attribute {0} at element {1}'.format(key, element.getroottree().getpath(element)))       
+      for child in list(element):
+        self.parse_element(child, parsed)
         return parsed
+       #Return a list of dictionaries from the text and attributes of the children under this XML root.
+    for child in root.getchildren():
+      return parse_element(child)
       
-    def process_data(self):
-        # Initiate the root XML, parse it, and return a dataframe #
-        structure_data = self.parse_root(self.root)
-        return pd.DataFrame(structure_data)
+  def process_data(self):
+  # Initiate the root XML, parse it, and return a dataframe #
+    structure_data = self.parse_root(self.root)
+    return pd.DataFrame(structure_data)
 
 xml2df = XML2DataFrame(xml_data)
 xml_dataframe = xml2df.process_data()
