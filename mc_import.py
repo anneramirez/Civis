1fcd22442498dcb16f1d54707c6b067c705ec046
anneramirez <anne.ramirez@ppfa.org>
2019-05-29 18:29:21 -0400

    mc_import_v2.py

--- mc_import.py
+++ mc_import.py
@@ -5,6 +5,9 @@ import json
 import os
 from requests.auth import HTTPBasicAuth
 import pandas as pd
+import xml.etree.ElementTree as ET
+import io
+
 
 url = "https://secure.mcommons.com/api/profiles"
 user = "anne.ramirez@ppfa.org"
@@ -19,9 +22,23 @@ payload = ""
 headers = {'Content-type' : 'application/json'}
 
 r = requests.request("GET", url, auth = HTTPBasicAuth(user,pw), data=payload, headers=headers, params=querystring)
-dict = xmltodict.parse(r.text)
-df = pd.DataFrame.from_dict(dict)
-print(df)
+xml_data = io.StringIO(r.text)
+
+def iter_docs(profile):
+    profile_attr = profile.attrib
+    for doc in profile.iter('document'):
+        doc_dict = profile_attr.copy()
+        doc_dict.update(doc.attrib)
+        doc_dict['data'] = doc.text
+        yield doc_dict
+        
+def iter_profile(etree):
+    for profile in etree.iter('profile'):
+        for row in iter_docs(profile):
+            yield row
+        
+etree = ET.parse(xml_data) #create an ElementTree object 
+doc_df = pd.DataFrame(list(iter_profile(etree)))
 
 
 #client = civis.APIClient()
