b78462aba027b5014b40ac3f0149343e7e2c737f
anneramirez <anne.ramirez@ppfa.org>
2019-05-30 17:05:14 -0400

    Update mc_looper.py

--- mc_looper.py
+++ mc_looper.py
@@ -21,25 +21,6 @@ params = {"include_custom_columns":"false",
               "include_clicks":"false",
               "include_members":"false",
               "page":"1"}
-dfcolumns = '['id','first_name','last_name',
-'phone_number',
-'email',
-'status',
-'created_at',
-'updated_at',
-'source_id',   
-'source_type',
-'source_name', 
-'source_opt_in_path_id',
-'opted_out_at',
-'outed_out_source',
-'address_street1',
-'address_street2',
-'address_city',
-'address_state',
-'address_postal_code',
-'address_country',
-'run_date']'
 
 
 #r = requests.request("GET", url, auth = HTTPBasicAuth(user,pw), data=payload, headers=headers, params=params)
