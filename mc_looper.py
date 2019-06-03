6e68bb72adbfacba279cc33c35f2081e98fa6442
anneramirez <anne.ramirez@ppfa.org>
2019-06-03 14:53:03 -0400

    Update mc_looper.py

--- mc_looper.py
+++ mc_looper.py
@@ -20,8 +20,8 @@ params = {'include_custom_columns':'false',
           'include_members':'false',
           'page':1,
           'company':14931,
-          #'to':'2018-12-12',
-          'from':'2019-01-01'} #include company id for C4
+          'to':'2018-12-12',
+          'from':'2018-01-01'} #include company id for C4
 
 
 ###Flatten###
