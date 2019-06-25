7df07a9114a5dd1f9b72c19c0fe5c918d4af433a
anneramirez <anne.ramirez@ppfa.org>
2019-06-25 13:34:16 -0400

    Update mc_profile.py

--- mc_profile.py
+++ mc_profile.py
@@ -7,6 +7,7 @@ import pandas as pd
 import os
 
 update_from = os.environ.get('update_from')
+update_to = os.environ.get('update_to')
 
 ### VAR Global ###
 user = "anne.ramirez+civisapi@ppfa.org"
@@ -19,7 +20,8 @@ params = {'include_custom_columns':'false',
                     'include_clicks':'true',
                     'include_members':'false',
                     'page':1,
-                    'from':update_from
+                    'from':update_from,
+                    'to':update_to
                   }
                   
 ### API Call ###
@@ -41,5 +43,60 @@ def flatten_dict(d, separator='_', prefix=''):
 def flatXML(tree):
     flat = [flatten_dict(x) for x in tree]
     return flat
-    
-    
+
+def process_sublist(t,obj):       
+    subs = []
+    single = {}
+    for p in t:
+        for k,v in p.items():
+            if k =='id':
+                try:
+                    path = p[obj+'s'][obj]
+                    if isinstance(path, dict): #this is single clicks
+                        single.update({'profile_id':v})
+                        for a,b in path.items():
+                            single.update({a:b})
+                        subs.append(single) 
+                        single = {}
+                    elif isinstance(path, list):
+                        for s in path:
+                            single.update({'profile_id':v})
+                            for a,b in s.items():
+                                single.update({a:b})
+                            subs.append(single) 
+                            single = {}
+                except Exception as ex:
+                    print(k,v,ex)
+                    continue                 
+    return subs
+
+def cleanPro(path):
+    for p in path:
+        del(p['clicks'])
+    return path
+  
+### PROFILES Loop through pages to get all results ###
+obj = 'profile'
+def loopPages(url,auth,params): 
+    recordsPro = []
+    recordsSub = []
+    while params['page'] < 3: #change to while True when done testing!!
+        try:
+            resp = getAPIdata(url,auth,params)
+            tree = processXML(resp)
+            path = tree['response'][obj+'s'][obj]
+            clicks = process_sublist(path,'click')
+            recordsSub.extend(clicks)
+            clean = cleanPro(path)
+            r = flatXML(clean)
+            recordsPro.extend(r)
+            #if (int(tree['response']['profiles']['num']) > 0):
+             #add data file to set
+            
+            params['page'] += 1 #go to next page
+            #else:
+             #   break
+        except:
+            break
+    params['page'] = 1
+    return recordsPro, recordsSub  
