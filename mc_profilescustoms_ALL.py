d0e6bb04c0e92afe63dce9908f9abe7a2ebf0823
anneramirez <anne.ramirez@ppfa.org>
2020-02-20 17:17:09 -0500

    Update mc_profilescustoms_ALL.py

--- mc_profilescustoms_ALL.py
+++ mc_profilescustoms_ALL.py
@@ -1,3 +1,38 @@
+import xmltodict
+import requests
+import civis
+import json
+from requests.auth import HTTPBasicAuth
+import pandas as pd
+import os
+import datetime
+
+today = datetime.date.today()
+yesterday = today - datetime.timedelta(days = 1)
+update_from = str(yesterday)+" 05:00:00 UTC"
+update_to = str(today)+" 04:59:59 UTC"
+
+user = os.environ.get('MC_CREDENTIAL_USERNAME')
+pw = os.environ.get('MC_CREDENTIAL_PASSWORD')
+company_key = os.environ.get('company_key')
+profiles_table = os.environ.get('profiles_table')
+clicks_table = os.environ.get('clicks_table')
+customs_table = os.environ.get('customs_table')
+subscriptions_table = os.environ.get('subscriptions_table')
+
+### VAR Global ###
+auth = HTTPBasicAuth(user,pw)
+url = "https://secure.mcommons.com/api/profiles"
+
+params = {'company':company_key,
+          'include_custom_columns':'true',
+          'include_subscriptions':'true',
+          'include_clicks':'true',
+          'include_members':'false',
+          'page':1,
+          'limit':1000        
+          }
+
 ### PROFILES Loop through pages to get all results ###
 obj = 'profile'
 def loopPages(url,auth,params): 
@@ -15,7 +50,7 @@ def loopPages(url,auth,params):
             r = flatXML(clean)
             rPro.extend(r)
             params['page'] += 1 #go to next page    
-            if params['page']%50 == 0: #evaluate current page
+            if params['page']%100 == 0: #evaluate current page
                 pushData(rPro,rCus)
                 rPro = []
                 rCus = []
