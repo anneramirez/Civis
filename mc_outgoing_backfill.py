861de65c5586d38cc3220cececf5cdfd9939e26e
anneramirez <anne.ramirez@ppfa.org>
2020-03-24 14:20:44 -0400

    Update mc_outgoing_backfill.py

--- mc_outgoing_backfill.py
+++ mc_outgoing_backfill.py
@@ -87,12 +87,12 @@ def loopPages(url,auth,params):
 def loopMonth (url,auth,params):
 	while start >= datetime.date(2020, 2, 1):
 		try:
-			loopPages(url,auth,params)  
-            print("Imported outgoing from " + str(start))
+			loopPages(url,auth,params)
+			print("Imported outgoing from " + str(start))
        		end = start
         	start = end - datetime.timedelta(days = 1)
-        	params.update( { 'start_time': str(start)+" 05:00:00 UTC",
-                      'end_time': str(end)+" 04:59:59 UTC" } )
+        	params.update({ 'start_time': str(start)+" 05:00:00 UTC",
+                      'end_time': str(end)+" 04:59:59 UTC" })
      	except:
         	break
 	print("Finished the month")
