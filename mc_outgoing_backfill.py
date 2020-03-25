7a6bbeaf48c28c5b18b89d8a76dc4ff45388bbf6
anneramirez <anne.ramirez@ppfa.org>
2020-03-25 11:10:43 -0400

    Update mc_outgoing_backfill.py

--- mc_outgoing_backfill.py
+++ mc_outgoing_backfill.py
@@ -104,11 +104,11 @@ def loopMonth (url,auth,params):
 			start = end - datetime.timedelta(days = 1)
 			params.update({ 'start_time': str(start)+" 05:00:00 UTC",
                       			'end_time': str(end)+" 04:59:59 UTC" })
-                except Exception as ex:
-                        print("Exception raised in looppages on page " + str(params['page']))
-                        print(ex)
-                        print("Unexpected error:", sys.exc_info()[0])
-                        break
+		 except Exception as ex:
+			print("Exception raised in looppages on page " + str(params['page']))
+			print(ex)
+			print("Unexpected error:", sys.exc_info()[0])
+			break
 	print("Finished the month")
 
 loopMonth (url,auth,params)
